654391f3fa2e24f1f7081f14b060b7d7bea746e2
alex-zwingli <alex.zwingli@outlook.com>
2016-11-25 14:17:50 -0700

    Insert old DAL folder

---
+++ BlowOut/BlowOut/DAL/AICContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AwesomeInstrumentCompany.Models;
+using System.Data.Entity;
+
+namespace AwesomeInstrumentCompany.DAL
+{
+    public class AICContext : DbContext 
+    {
+        public AICContext () : base("AICContext")
+        {
+
+        }
+
+        //Data structure that stores data
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Instrument> Instruments { get; set; }
+        public DbSet<Client_Instrument> Clients_Instruments { get; set; }
+    }
+}
