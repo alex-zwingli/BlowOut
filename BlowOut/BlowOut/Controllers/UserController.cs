bc4ee2eaa71e6e20c7d6c63efd8173119765fad8
alex-zwingli <alex.zwingli@outlook.com>
2016-11-27 04:20:41 -0700

    Added Modal

---
+++ BlowOut/BlowOut/Controllers/UserController.cs
@@ -0,0 +1,51 @@
+﻿using ModalLogin.BL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlowOut.Controllers
+{
+    public class UserController : Controller
+    {
+        UserBusinessLogic UserBL = new UserBusinessLogic();
+        //
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(User User)
+        {
+            string message = "";
+            if (ModelState.IsValid)
+            {
+                if (UserBL.CheckUserLogin(User)>0)
+                {
+                    message = "Success";
+                }
+                else
+                {
+                    message = "UserName Or Password Is Incorrect";
+                }
+            }
+            else
+            {
+                message = "All Fields Required";
+            }
+
+            if (Request.IsAjaxRequest())
+            {
+                return Json(message, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+        }
+    }
+}
---
+++ BlowOut/ModalLogin.BL/User.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModalLogin.BL
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+
+    public class UserBusinessLogic
+    {
+        string conStrt = ConfigurationManager.ConnectionStrings["AICContext"].ConnectionString;
+        public int CheckUserLogin(User User)
+        {
+            using (SqlConnection conObj = new SqlConnection(conStrt))
+            {
+                SqlCommand comObj = new SqlCommand("uspLogin", conObj);
+                comObj.CommandType = CommandType.StoredProcedure;
+                comObj.Parameters.Add(new SqlParameter("@UserName", User.UserName));
+                comObj.Parameters.Add(new SqlParameter("@Password", User.Password));
+                conObj.Open();
+                return Convert.ToInt32(comObj.ExecuteScalar());
+
+
+
+            }
+
+        }
+    }
+}
